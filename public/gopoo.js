23ed0ac677ec34c5d894f02cef6154f1883749d4
mrfair <mr.fair@msn.com>
2017-06-23 22:33:02 +0700

    Add files via upload

--- public/gopoo.js
+++ public/gopoo.js
@@ -4,16 +4,11 @@ var gp = {
   },
   position : {
     load : function() {
-      $.ajax({
-        url: "https://maps.googleapis.com/maps/api/place/radarsearch/json?location=14.0347533,100.7378522&radius=10000 &keyword=ห้องน้ำ&language=th&key=AIzaSyDFU2TVjDLzdx0Q7nukpwA1xg-SHMwI8fY"
-      }).done(function( j ) {
-        console.log( j );
-      });
+
     }
   }
 };
 
 $( function(){
-  console.log( "สวัสดีครับ ยินดีต้องรับสู่ GoPoo" );
-  gp.position.load();
+  console.log( "สวัสดีครับ ยินดีต้องรับสู่ GoPoo" ); 
 });
