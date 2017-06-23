10a92fb3f2c1af12bbd42bb36faa4abbcfa6097a
mrfair <mr.fair@msn.com>
2017-06-23 21:16:27 +0700

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+//importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(event) {
+  /*
+  event.waitUntil(
+    caches.open('demo-cache').then(function(cache) {
+      return cache.put('/', new Response("From the cache!"));
+    })
+  );
+  */
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response;
+    })
+  );
+});
