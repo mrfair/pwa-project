c8b15272eefa5bd650910c635005dde8fa9f70a2
mrfair <mr.fair@msn.com>
2017-06-24 08:20:51 +0700

    Add files via upload

--- public/gopoo.js
+++ public/gopoo.js
@@ -6,9 +6,28 @@ var gp = {
     load : function() {
 
     }
+  },
+  detail : function() {
+
+  },
+  gguserphoto : function( p ) {
+    if( p[0].html_attributions[0] ) {
+      var link = $( p[0].html_attributions[0] ).attr( "href" );
+      return '<a target="_blank" href="' + link + '" target="gguserframe">ภาพบรรยากาศ</a>';
+    }
   }
 };
 
 $( function(){
-  console.log( "สวัสดีครับ ยินดีต้องรับสู่ GoPoo" ); 
+  console.log( "สวัสดีครับ ยินดีต้องรับสู่ GoPoo" );
+
+  $("#rateYo").rateYo({
+    rating    : 2.5,
+    spacing   : "3px",
+    starWidth: "14px",
+    multiColor: {
+       "startColor": "#FF0000", //RED
+       "endColor"  : "#00FF00"  //GREEN
+    }
+  });
 });
--- public/sw.js
+++ public/sw.js
@@ -2,8 +2,16 @@ importScripts('/cache-polyfill.js');
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('demo-cache').then(function(cache) {
-      return cache.put('/', new Response("From the cache!"));
+    caches.open('demo-cache')
+    .then(function(cache) {
+      return cache.addAll([
+          '/',
+          '/style.css',
+          '/gopoo.js'
+        ]);
+    })
+    .then(function() {
+      console.log('WORKER: install completed');
     })
   );
 });
